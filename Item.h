42eac81d1e68d3c64fd3521c43f134968ddedda1
IT21040726 <87372832+it21040726@users.noreply.github.com>
2021-08-25 13:20:08 +0000

    all question completed

--- Item.h
+++ Item.h
@@ -5,8 +5,12 @@ class Item {
       int itemCode;
       float unitPrice;
       float discount; // out of 100 e.g. discount = 15
+
+      
    public:
-      // 1. Write the Default Constructor
+      Item();
+      Item(int icode, float unitPrice);
+      ~Item();// 1. Write the Default Constructor
       // 2. Write the Overloaded Constructor
       // 3. Write the Destructor
 
